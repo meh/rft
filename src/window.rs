bf6c9f096b98326b3639d1534085c4fd030940dd
meh <meh@schizofreni.co>
2015-08-04 20:47:17 +0200

    window: add in-place hamming

--- src/window.rs
+++ src/window.rs
@@ -1,6 +1,6 @@
 use std::f64::consts::PI;
 
-use {Precision, Sample};
+use {Precision, Sample, SampleMut};
 use strided::{Strided, MutStrided};
 
 pub fn hamming<S, I>(input: I) -> Vec<Precision>
@@ -30,5 +30,21 @@ pub fn hamming_in<S, I, O>(input: I, mut output: O)
 		*output = input.normalize()
 			* (0.54 - 0.46)
 			* (((PI * 2.0) as Precision * i as Precision) / (length - 1.0));
+	}
+}
+
+pub fn hamming_on<S, IO>(mut data: IO)
+	where S:  SampleMut,
+	      IO: MutStrided<Elem=S>
+{
+	let mut data   = data.as_stride_mut();
+	let     length = data.len() as Precision;
+
+	for (i, datum) in data.iter_mut().enumerate() {
+		let new = datum.normalize()
+			* (0.54 - 0.46)
+			* (((PI * 2.0) as Precision * i as Precision) / (length - 1.0));
+
+		datum.set_normalized(new);
 	}
 }
